c1a5d97683d5b2c0d2d0374a0e68c317e2eaaf05
mhong124 <mhong124@terpmail.umd.edu>
2024-07-05 17:34:10 -0400

    updated profile

--- app/screens/ProfileScreen.js
+++ app/screens/ProfileScreen.js
@@ -9,14 +9,35 @@ import {
   View,
   Button,
   Alert,
+  Image,
+  Pressable,
+  ScrollView,
 } from "react-native";
 
 function ProfileScreen(props) {
   return (
-    <SafeAreaView style = {styles.container}>
+    <SafeAreaView style = {[styles.container]}>
+      <ScrollView>
       <Text style = {[styles.heading]}>
         Profile
-      </Text>  
+      </Text>
+      <Pressable style = {styles.profileContainer} title = "Profile Edit">
+        <Image style = {styles.pfp} source = {require('../assets/sample.png')}></Image>
+        <View style = {styles.profileInfo}>
+          <Text style = {styles.name}>Name</Text>
+          <Text style = {styles.workouts}># of workouts</Text>
+        </View>
+      </Pressable>
+      <Text style = {styles.name}>Dashboard</Text>
+      <Pressable style = {styles.widget}>
+        <Text style = {styles.name}>Workouts Per Week</Text>
+        <Text style = {styles.workouts}>Activity</Text>
+      </Pressable>
+      <Pressable style = {styles.widget}>
+        <Text style = {styles.name}>Weight</Text>
+        <Text style = {styles.workouts}>Absolute</Text>
+      </Pressable> 
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -24,13 +45,51 @@ function ProfileScreen(props) {
 const styles = StyleSheet.create({
   heading: {
     fontSize: 30,
-    fontWeight: 600,
+    fontWeight: 800,
   },
 
   container: {
+    flex: 1,
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
     margin: 10,
+    justifyContent: 'space-between',
+    flexDirection: 'column',
   },
+
+  profileContainer: {
+    flex: 1,
+    margin: 30,
+    flexDirection: 'row',
+  },
+
+  pfp: {
+    height: 50,
+    width: 50,
+  },
+
+  profileInfo: {
+    flex: 1,
+    margin: 20,
+    flexDirection: 'column',
+  },
+
+  name: {
+    fontWeight: 800,
+    fontSize: 20,
+  },
+
+  workouts: {
+    fontSize: 20,
+    color: 'grey',
+  },
+
+  widget: {
+    borderWidth: 2,
+    borderRadius: 5,
+    marginTop: 20,
+    padding: 10,
+    height: 200,
+  }
 });
 
 export default ProfileScreen;
