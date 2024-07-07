819fbd57697a3dc453e8ffbc4ccecc5b1e86b701
Minsi Hu <101283845+Mimsqueeze@users.noreply.github.com>
2024-07-06 20:15:26 -0400

    navbar done

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import {} from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ProfileScreen from "./app/screens/ProfileScreen";
 import ExerciseScreen from "./app/screens/ExerciseScreen";
@@ -9,48 +10,19 @@ import MeasureScreen from "./app/screens/MeasureScreen";
 import TimerScreen from "./app/screens/TimerScreen";
 import WorkoutScreen from "./app/screens/WorkoutScreen";
 
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Welcome"
-          component={WelcomeScreen}
-          options={{ headerLeft: null, headerShown: false }}
-        />
-        <Stack.Screen
-          name="Profile"
-          component={ProfileScreen}
-          options={{ headerBackVisible: false }}
-        />
-        <Stack.Screen
-          name="Exercise"
-          component={ExerciseScreen}
-          options={{ headerBackVisible: false }}
-        />
-        <Stack.Screen
-          name="History"
-          component={HistoryScreen}
-          options={{ headerBackVisible: false }}
-        />
-        <Stack.Screen
-          name="Measure"
-          component={MeasureScreen}
-          options={{ headerBackVisible: false }}
-        />
-        <Stack.Screen
-          name="Timer"
-          component={TimerScreen}
-          options={{ headerBackVisible: false }}
-        />
-        <Stack.Screen
-          name="Workout"
-          component={WorkoutScreen}
-          options={{ headerBackVisible: false }}
-        />
-      </Stack.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+        <Tab.Screen name="History" component={HistoryScreen} />
+        <Tab.Screen name="Exercise" component={ExerciseScreen} />
+        <Tab.Screen name="Timer" component={TimerScreen} />
+        <Tab.Screen name="Measure" component={MeasureScreen} />
+        <Tab.Screen name="Workout" component={WorkoutScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
