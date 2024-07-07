25f1e2272fd15bf0cfe55def75629e751ae8e1b4
Minsi Hu <101283845+Mimsqueeze@users.noreply.github.com>
2024-07-06 20:43:14 -0400

    navbar complete

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import {} from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
 
-import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ProfileScreen from "./app/screens/ProfileScreen";
 import ExerciseScreen from "./app/screens/ExerciseScreen";
 import HistoryScreen from "./app/screens/HistoryScreen";
-import MeasureScreen from "./app/screens/MeasureScreen";
 import TimerScreen from "./app/screens/TimerScreen";
 import WorkoutScreen from "./app/screens/WorkoutScreen";
 
@@ -15,13 +14,30 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route, navigation }) => ({
+          tabBarIcon: ({ color, focused, size }) => {
+            let iconName;
+            if (route.name === "Profile") {
+              iconName = focused ? "person-circle" : "person-circle-outline";
+            } else if (route.name === "History") {
+              iconName = focused ? "bookmarks" : "bookmarks-outline";
+            } else if (route.name === "Workout") {
+              iconName = focused ? "add-circle" : "add-circle-outline";
+            } else if (route.name === "Exercise") {
+              iconName = focused ? "barbell" : "barbell-outline";
+            } else if (route.name === "Timer") {
+              iconName = focused ? "timer" : "timer-outline";
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+      >
         <Tab.Screen name="Profile" component={ProfileScreen} />
         <Tab.Screen name="History" component={HistoryScreen} />
+        <Tab.Screen name="Workout" component={WorkoutScreen} />
         <Tab.Screen name="Exercise" component={ExerciseScreen} />
         <Tab.Screen name="Timer" component={TimerScreen} />
-        <Tab.Screen name="Measure" component={MeasureScreen} />
-        <Tab.Screen name="Workout" component={WorkoutScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
