2cb5bd97c8e6dc615a3d01044e9b7a5eeac2e9e0
Minsi Hu <101283845+Mimsqueeze@users.noreply.github.com>
2024-07-16 18:32:42 -0400

    exercises reformatted

--- app/screens/ExerciseScreen.js
+++ app/screens/ExerciseScreen.js
@@ -1,72 +1,51 @@
 import React from "react";
-import {
-  StyleSheet,
-  Text,
-  Platform,
-  StatusBar,
-  SafeAreaView,
-  TouchableNativeFeedback,
-  View,
-  Button,
-  Alert,
-  Image,
-  Pressable,
-  ScrollView,
-} from "react-native";
+import { SafeAreaView, ScrollView, Platform, StatusBar } from "react-native";
+import styled from "styled-components/native";
+import { Header, SubHeader } from "../config/style";
+import Exercise from "../components/Exercise";
 
-import { AlphabetList } from "react-native-section-alphabet-list";
+const exerciseData = require("../data/exerciseData.json");
 
-import { exercises } from "../data/Exercise";
-// const exercises = require("../data/exercises.json");
+const sortedExercises = exerciseData.sort((a, b) =>
+  a.name.localeCompare(b.name)
+);
 
 function ExerciseScreen() {
+  let currentLetter = "";
+
   return (
-    <SafeAreaView>
-      <AlphabetList
-        data={exercises}
-        indexLetterStyle={{ color: "lightblue", fontSize: 15 }}
-        renderCustomItem={(item) => (
-          <View style={styles.divide}>
-            <Pressable style={styles.item}>
-              <Image style={styles.img} source={item.photo}></Image>
-              <Text style={styles.text}>{item.value}</Text>
-            </Pressable>
-            <Text style={styles.desc}>{item.description}</Text>
-          </View>
-        )}
-      />
-    </SafeAreaView>
+    <SafeContainer>
+      <ScrollView>
+        <Header>Exercises</Header>
+        {sortedExercises.map((exercise, index) => {
+          const firstLetter = exercise.name.charAt(0).toUpperCase();
+          const showSubHeader = firstLetter !== currentLetter;
+          if (showSubHeader) {
+            currentLetter = firstLetter;
+          }
+          return (
+            <React.Fragment key={exercise.key}>
+              {showSubHeader && <SubHeader>{firstLetter}</SubHeader>}
+              <Exercise
+                name={exercise.name}
+                photo={exercise.photo}
+                description={exercise.description}
+                muscles={exercise.muscles}
+              />
+            </React.Fragment>
+          );
+        })}
+      </ScrollView>
+    </SafeContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  item: {
-    flex: 1,
-    flexDirection: "row",
-    margin: 20,
-  },
-
-  img: {
-    height: 50,
-    width: 50,
-    verticalAlign: "middle",
-  },
-  text: {
-    margin: 10,
-    fontSize: 20,
-  },
-  desc: {
-    margin: 15,
-    justifyContent: "center",
-    alignItems: "center",
-    textAlign: "center",
-  },
-
-  divide: {
-    borderBottomWidth: 1,
-    borderBottomColor: "grey",
-    marginHorizontal: 7,
-  },
-});
+const SafeContainer = styled(SafeAreaView)`
+  flex: 1;
+  padding-top: ${Platform.OS === "android" ? StatusBar.currentHeight : 0}px;
+  margin: 15px;
+  justify-content: space-between;
+  flex-direction: column;
+`;
 
 export default ExerciseScreen;
